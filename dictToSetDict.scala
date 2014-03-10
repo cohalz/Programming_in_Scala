9b73eb61965a1f05086985602e5ec5d5a50c68a5
cohalz <cohalzweb@gmail.com>
2014-03-10 12:02:02 +0900

    files classified into chapter

--- dictToSetDict.scala
+++
@@ -1,7 +0,0 @@
-import scala.io.Source
-if(args.length > 0) {
-  val dict = Source.fromFile(args(0)).getLines().toList
-	val dictsorted = dict.sorted.distinct
-	dictsorted.foreach(println)
-}
-else Console.err.println("Please enter inputfile")
